881281ee6085cc97b57390b78f32331144b63c58
star-reader <jinch2287@outlook.com>
2025-12-05 20:56:47 +0800

    feat: Implement drawOnlineController service for displaying online controllers and their coverage areas

---
+++ src/services/map/layers/drawOnlineController.ts
@@ -0,0 +1,13 @@
+/**
+ * drawOnlineController Service
+ * 
+ * 显示在线管制员，包括CTR、APP、FSS的polygen和其他的点+圆圈辐射范围
+ * 
+ * @author Jerry Jin
+ * @date 2025-12-05
+ */
+
+export default () => {
+    // todo 周六活动有conteolelrs再写这个模块
+    return null
+}
