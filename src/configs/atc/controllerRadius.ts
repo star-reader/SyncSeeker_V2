1106d0c324ff3545a6e63aa3817ad86f7bdf2cb6
star-reader <jinch2287@outlook.com>
2025-12-20 21:20:43 +0800

    feat: implement dynamic controller colors based on theme; add controller radius configurations

---
+++ src/configs/atc/controllerRadius.ts
@@ -0,0 +1,11 @@
+export const CIRCLE_RADIUS_METERS: Record<ControllerType, number> = {
+    FSS: 1092800,   // ~590 NM
+    CTR: 441120,    // ~238 NM
+    APP: 221520,    // ~120 NM
+    TWR: 10820,     // ~5.8 NM
+    GND: 3700,      // ~2 NM
+    DEL: 2408,      // ~1.3 NM
+    ATIS: 0,
+    OBS: 0,
+    OTHER: 5000
+}
